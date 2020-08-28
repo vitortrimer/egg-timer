2f406788637a9bce790a6ec10226d6c77a0a8c41
Vitor <vitor.trimer@ringa.com.br>
2020-08-28 19:36:55 -0300

    London app brewery egg timer tutorial

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var titleLabel: UILabel!
+    let eggTimes = [
+        "Soft": 300,
+        "Medium": 420,
+        "Hard": 720
+    ]
+    var timer = Timer()
+    var totalTime = 0
+    var secondsPassed = 0
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        titleLabel.text = "How do you like your eggs?"
+        timer.invalidate()
+        progressBar.progress = 0
+        secondsPassed = 0
+        let hardness = sender.currentTitle!
+        
+        totalTime = eggTimes[hardness]!
+        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
 
+    
+    @objc func updateTimer() {
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            let percentageProgress = Float(secondsPassed) / Float(totalTime)
+            progressBar.progress = percentageProgress
+        } else {
+            timer.invalidate()
+            titleLabel.text = "DONE!"
+        }
+    }
+    
 
 }
